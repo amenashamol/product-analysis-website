8c0cbf2cfe1b00c68ff0d36f341a82069a76cea5
amena <amena.ruet@gmail.com>
2022-04-03 23:16:57 +0600

    customlink

---
+++ src/component/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import CustomLink from '../CustomLink/CustomLink';
+
+
+import './Header.css'
+const Header = () => {
+    return (
+       <div >
+             
+           <nav>
+           <CustomLink to="/">Home</CustomLink>
+               <CustomLink to="/reviews">Reviews</CustomLink>
+               <CustomLink to="/dashboard">Dashboard</CustomLink>
+               <CustomLink to="/blogs">Blogs</CustomLink>
+               <CustomLink to="/about">About</CustomLink>
+           </nav>
+     
+        </div>
+    );
+};
+
+export default Header;
